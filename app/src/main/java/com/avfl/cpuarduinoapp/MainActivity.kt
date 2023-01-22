f9bc744a29a5410d4e1b97a632a0416a55203a07
AvFl4028 <aviel4028@gmail.com>
2023-01-21 18:22:45 -0600

    first

---
+++ app/src/main/java/com/avfl/cpuarduinoapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.avfl.cpuarduinoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
